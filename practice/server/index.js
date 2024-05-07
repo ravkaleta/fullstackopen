945b3210f80d43bb80d1e2fc40406e7d5239fb4e
ravkaleta <164653707+ravkaleta@users.noreply.github.com>
2024-05-07 10:15:10 +0200

    Removing unnecessary files and setting environment variables

--- practice/server/index.js
+++ practice/server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -91,7 +92,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint);
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
